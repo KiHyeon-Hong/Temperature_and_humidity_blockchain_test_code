e14632ac83847bd2f4501b7d07b40bfc123b6283
Ki Hyeon Hong <70433404+KiHyeon-Hong@users.noreply.github.com>
2022-06-10 01:41:14 +0900

    20220610 update code

--- DHT22.js
+++ DHT22.js
@@ -1,4 +1,4 @@
-// const sensorLib = require('node-dht-sensor');
+const sensorLib = require('node-dht-sensor');
 
 function getDate() {
   const d = new Date();
@@ -8,31 +8,31 @@ function getDate() {
 
 // 온습도 데이터 측정 class
 class DHT22 {
-  // async get() {
-  //   const myData = new Promise((resolve) => {
-  //     const sensor = {
-  //       sensors: [
-  //         {
-  //           name: 'Outdoor',
-  //           type: 22,
-  //           pin: 21,
-  //         },
-  //       ],
-  //       read: function () {
-  //         for (var index in this.sensors) {
-  //           var s = sensorLib.read(this.sensors[index].type, this.sensors[index].pin);
-  //           resolve({ temperature: parseFloat(s.temperature.toFixed(1)), humidity: parseFloat(s.humidity.toFixed(1)), date: getDate(), room: 219 });
-  //         }
-  //       },
-  //     };
-  //     sensor.read();
-  //   });
-  //   return await myData;
-  // }
-
   async get() {
-    return await { temperature: 20.5, humidity: 50.2, date: getDate(), room: 220 };
+    const myData = new Promise((resolve) => {
+      const sensor = {
+        sensors: [
+          {
+            name: 'Outdoor',
+            type: 22,
+            pin: 21,
+          },
+        ],
+        read: function () {
+          for (var index in this.sensors) {
+            var s = sensorLib.read(this.sensors[index].type, this.sensors[index].pin);
+            resolve({ temperature: parseFloat(s.temperature.toFixed(1)), humidity: parseFloat(s.humidity.toFixed(1)), date: getDate(), room: 219 });
+          }
+        },
+      };
+      sensor.read();
+    });
+    return await myData;
   }
+
+  // async get() {
+  //   return await { temperature: 21.4, humidity: 51.8, date: getDate(), room: 513 };
+  // }
 }
 
 module.exports.DHT22 = DHT22;
